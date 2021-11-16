ee01a73da63b6fe3cd37d5a4c07330101f418449
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2021-11-16 13:44:52 +0530

    :sparkles:get specified fields and pagination

--- controller/products.js
+++ controller/products.js
@@ -3,7 +3,7 @@ const productModel = require('../models/products');
 
 const getProducts = asyncWrapper(async(req,res,next) => {
     const searchQuery = {};
-    const {company,featured,name} = req.query;
+    const {company,featured,name,sort, fields,pageNumber,limitNumber} = req.query;
     if(featured){
         searchQuery.featured = featured === 'true' ? true:false;
     }
@@ -13,7 +13,24 @@ const getProducts = asyncWrapper(async(req,res,next) => {
     if(name){
         searchQuery.name = {$regex:name, $options:'i'};
     }
-    const products = await productModel.find(searchQuery);
+    let toSend = productModel.find(searchQuery);
+    if(sort){
+        let sorted = sort.split(',').join(' ');
+        toSend = toSend.sort(sorted);
+    }else{
+        toSend = toSend.sort('createdAt');
+    }
+    if(fields){
+        const fieldList = fields.split(',').join(' ');
+        toSend = toSend.select(fieldList);
+    }
+    //for page and limits
+    let page = Number(pageNumber) || 1;
+    let limit = Number(limitNumber) || 10;
+    let skip = (page-1)*limit;
+    toSend = toSend.skip(skip).limit(limit);
+    
+    const products = await toSend;
     res.status(200).json({nbhits:products.length,products});
 });
 
